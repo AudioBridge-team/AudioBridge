a78cb39b39593685889fabe7a95c09a168a1d277
Shon <shonzbso@gmail.com>
2022-08-17 17:50:16 +0500

    Adding arguments for set version tag

--- install.zsh
+++ install.zsh
@@ -1,2 +1,11 @@
-mkdir perfect
-echo "Completed"
+while getopts v: flag
+do
+	case "${flag}" in
+		v) version=${OPTARG};;
+	esac
+done
+if [ -z "$version" ]; then
+	echo 'ERROR: Version is empty! Set it with argument "-v"'
+	exit 1
+fi
+echo "Version is $version";
