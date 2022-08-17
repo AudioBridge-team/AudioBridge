72547059523fdc18a94e50ce65c8db2664c718ea
Shon <shonzbso@gmail.com>
2022-08-17 22:13:02 +0500

    Pre-release version

--- install.zsh
+++ install.zsh
@@ -1,11 +1,27 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+#Получение значения версии
 while getopts v: flag
 do
 	case "${flag}" in
 		v) version=${OPTARG};;
 	esac
 done
+#Проверка на наличие версии, в случае отсутсвия — заверешение
 if [ -z "$version" ]; then
 	echo 'ERROR: Version is empty! Set it with argument "-v"'
 	exit 1
 fi
-echo "Version is $version";
+#Удаление старого контейнера
+echo 'Update in progress.'
+docker rm --force vkbot_container
+docker build -t vkbot_container .
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+#Запуск нового контейнера
+echo 'Docker: starting up...'
+docker run --env-file /root/AudioBridge-data/.env -e VERSION="$version" -v /root/AudioBridge-data:/var/lib/AudioBridge-sql --name vkbot_container -d vkbot_container
+
+echo "Docker status:"
+echo "$(docker ps)"
+echo "$(docker images)"
