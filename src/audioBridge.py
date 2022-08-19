21c55234e77b3cdd8669e179baea1deb377c829b
Shon <shonzbso@gmail.com>
2022-08-19 15:37:51 +0500

    Revert "Added saving logs"
    This reverts commit d94011fcbae54bdcf444c789628140c150317d75.

--- src/audioBridge.py
+++ src/audioBridge.py
@@ -636,24 +636,22 @@ class VkBotWorker():
 
 
 if __name__ == '__main__':
+	logger = logging.getLogger('logger')
+	logger.setLevel(logging.DEBUG)
+	handler = StreamHandler(stream = sys.stdout)
+	handler.setFormatter(
+		Formatter(
+			#fmt = '[%(asctime)s, %(levelname)s] ~ %(threadName)s (%(funcName)s)\t~: %(message)s',
+			fmt = '[%(asctime)s, %(levelname)s] ~ (%(funcName)s)\t~: %(message)s',
+			datefmt = time.strftime('%d-%m-%y %H:%M:%S')
+		)
+	)
+	logger.addHandler(handler)
+
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-v", "--version", help="Version of the bot")
 	parser.add_argument("-d", "--debug", action='store_true', help="Debug mode")
 	args = parser.parse_args()
-
-	path_logs = "/AudioBridge-data/logs/logs-release.log"
-	if args.debug:
-		path_logs = "/AudioBridge-data/logs/logs-debug.log"
-	os.makedirs(os.path.dirname(path_logs), exist_ok=True)
-
-	logger = logging.getLogger('logger')
-	logging.basicConfig(filename=path_logs,
-                    	filemode='a',
-						encoding='utf-8',
-						#format = '[%(asctime)s, %(levelname)s] ~ %(threadName)s (%(funcName)s)\t~: %(message)s',
-						format = '[%(asctime)s, %(levelname)s] ~ (%(funcName)s)\t~: %(message)s',
-						datefmt = '%d-%m-%y %H:%M:%S',
-						level=logging.DEBUG)
 
 	logger.info('Program started.')
 	logger.info(f'Debug mode is {args.debug}')
