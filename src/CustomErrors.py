f6f6354ed3496a41abf9e95931e1118ba19724ae
Shon <shonzbso@gmail.com>
2022-08-19 23:38:14 +0500

    Add ArgumentParser

--- src/CustomErrors.py
+++ src/CustomErrors.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
-CONST_NAME = "Name"
-
+import argparse
 
 class CustomError(Exception):
 	def __init__(self, text):
 		self.txt = text
 
+class ArgumentParser(argparse.ArgumentParser):
+    def error(self, message):
+        raise Exception(message)
