467821a76f9af3f2106dd89e5b05c49f0a984a7c
Shon <shonzbso@gmail.com>
2022-08-19 14:30:54 +0500

    Fixing vars

--- install.zsh
+++ install.zsh
@@ -2,41 +2,41 @@
 # -*- coding: utf-8 -*-
 
 #Объявление переменных
-args=""
-version=""
-debug_mode=false
-container_name="vkbot_container"
+ARGS=""
+VERSION=""
+DEBUG_MODE=false
+CONTAINER_NAME="vkbot_container"
 #Получение значения версии
 while getopts v:d flag
 do
 	case "${flag}" in
-		v) version=${OPTARG};;
-		d) debug_mode=true;;
+		v) VERSION=${OPTARG};;
+		d) DEBUG_MODE=true;;
 		*) echo "Invalid option: -$flag";;
 	esac
 done
 #Проверка на статус режима отладки
-if [ "$debug_mode" = true ]; then
-	container_name="vkbot_debug_container"
-	version="vDebug"
-	args="--debug "
+if [ "$DEBUG_MODE" = true ]; then
+	CONTAINER_NAME="vkbot_debug_container"
+	VERSION="vDebug"
+	ARGS="--debug "
 fi
 #Проверка на наличие версии, в случае отсутсвия — заверешение
-if [ -z "$version" ]; then
-	echo 'ERROR: Version is empty! Set it with argument "-v, --version"'
+if [ -z "$VERSION" ]; then
+	echo 'ERROR: version is empty! Set it with argument "-v, --version"'
 	exit 1
 fi
 
-args+='--version "$version"'
+ARGS+="--version $VERSION"
 
 #Удаление старого контейнера
 echo "Update in progress."
-docker rm --force "$container_name"
-docker build -t "$container_name" .
+docker rm --force "$CONTAINER_NAME"
+docker build -t "$CONTAINER_NAME" .
 docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
 #Запуск нового контейнера
-echo "Docker: starting up $container_name..."
-docker run --env-file /root/AudioBridge-data/.env --volume /root/AudioBridge-data:/var/lib/AudioBridge-sql --name "$container_name" --detach "$container_name" args
+echo "Docker: starting up $CONTAINER_NAME..."
+docker run --env-file /root/AudioBridge-data/.env --volume /root/AudioBridge-data:/var/lib/AudioBridge-sql --name "$CONTAINER_NAME" --detach "$CONTAINER_NAME" "$ARGS"
 
 echo "Docker status:"
 echo "$(docker ps)"
