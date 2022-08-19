79b197f75b9ee4d3151a013d057d629538f06fed
Shon <shonzbso@gmail.com>
2022-08-19 14:40:40 +0500

    Fixing arguments line

--- install.zsh
+++ install.zsh
@@ -4,30 +4,27 @@
 #Объявление переменных
 ARGS=""
 VERSION=""
-DEBUG_MODE=false
+DEBUG=""
 CONTAINER_NAME="vkbot_container"
 #Получение значения версии
 while getopts v:d flag
 do
 	case "${flag}" in
 		v) VERSION=${OPTARG};;
-		d) DEBUG_MODE=true;;
+		d) DEBUG="--debug";;
 		*) echo "Invalid option: -$flag";;
 	esac
 done
 #Проверка на статус режима отладки
-if [ "$DEBUG_MODE" = true ]; then
+if [ ! -z "$DEBUG" ]; then
 	CONTAINER_NAME="vkbot_debug_container"
 	VERSION="vDebug"
-	ARGS="--debug "
 fi
 #Проверка на наличие версии, в случае отсутсвия — заверешение
 if [ -z "$VERSION" ]; then
 	echo 'ERROR: version is empty! Set it with argument "-v, --version"'
 	exit 1
 fi
-
-ARGS+="--version $VERSION"
 
 #Удаление старого контейнера
 echo "Update in progress."
@@ -36,7 +33,7 @@ docker build -t "$CONTAINER_NAME" .
 docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
 #Запуск нового контейнера
 echo "Docker: starting up $CONTAINER_NAME..."
-docker run --env-file /root/AudioBridge-data/.env --volume /root/AudioBridge-data:/var/lib/AudioBridge-sql --name "$CONTAINER_NAME" --detach "$CONTAINER_NAME" "$ARGS"
+docker run --env-file /root/AudioBridge-data/.env --volume /root/AudioBridge-data:/var/lib/AudioBridge-sql --name "$CONTAINER_NAME" --detach "$CONTAINER_NAME" --version "$VERSION" "$DEBUG"
 
 echo "Docker status:"
 echo "$(docker ps)"
