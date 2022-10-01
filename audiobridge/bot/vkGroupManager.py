cb35d164b27cf5cb5d416f6e4a33f8633f2597af
Shon <shonzbso@gmail.com>
2022-10-02 00:26:46 +0500

    Add using pandoc

--- audiobridge/bot/vkGroupManager.py
+++ audiobridge/bot/vkGroupManager.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import subprocess
 from audiobridge.common import vars
 from audiobridge.common.config import VkGroup, BotAuth
+from audiobridge.tools.customErrors import CustomError
 
 logger = logging.getLogger('logger')
 vkgroup_conf = VkGroup()
@@ -16,18 +18,31 @@ class VkGroupManager():
 		if vkgroup_conf.SYNC_CHANGELOG:
 			self._sync_changelog()
 
-	def _convert_to_wiki(self, body: str) -> str:
-		return body
+	def _fix_wiki_for_vk(self, wiki: str) -> str:
+		# удаление переноса строки после заголовков
+		# добавление ещё одного знака = для заголовков
+		# добавление знака | в заголовках с ссылками
+		return wiki
 
 	def _sync_changelog(self):
-		if vkgroup_conf.CHANGELOG_PAGE_ID == -1:
-			logger.error("Can't get changelog page id. Changelog wasn't synchronized!")
-			return
 		try:
-			changelog = ""
-			with open("CHANGELOG.md", "r", encoding="utf-8") as file:
-				changelog = self._convert_to_wiki(file.read())
-			logger.debug(changelog)
-			# vars.vk_agent.pages.save(text = "ok", page_id=vkgroup_conf.CHANGELOG_PAGE_ID, group_id=auth_conf.BOT_ID)
+			if vkgroup_conf.CHANGELOG_PAGE_ID == -1:
+				raise CustomError("Can't get changelog page id. Changelog wasn't synchronized!")
+
+			# proc = subprocess.Popen("pandoc CHANGELOG.md -t mediawiki", stdout = subprocess.PIPE, stderr = subprocess.PIPE, text = True, shell = True)
+			# stdout, stderr = proc.communicate()
+			# if stderr:
+			# 	raise CustomError(f"Can't convert .md to .wiki: {stderr}")
+			# if not stdout:
+			# 	raise CustomError('CHANGELOG.wiki is empty!')
+
+			# vars.vk_agent.pages.save(text=stdout, page_id=vkgroup_conf.CHANGELOG_PAGE_ID, group_id=auth_conf.BOT_ID)
+
+			with open("CHANGELOG.wiki", "r", encoding="utf-8") as file:
+				wiki = file.read()
+				logger.debug(self._fix_wiki_for_vk)
+
+		except CustomError as er:
+			logger.error(f'Custom: {er}')
 		except Exception as er:
 			logger.error(f"Can't update vk changelog: {er}")
