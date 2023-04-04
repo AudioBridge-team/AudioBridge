876f694e6ab05a8664681f88c391f4dad7778199
Shon <shonzbso@gmail.com>
2023-04-04 16:48:06 +0500

    Update table schema

--- audiobridge/db/scripts/init_tables.sql
+++ audiobridge/db/scripts/init_tables.sql
@@ -1,22 +1,37 @@
-CREATE TABLE if not exists audio_requests (
-    req_id INTEGER PRIMARY KEY,                 -- id запроса (сообщения в вк)
-    audio_id INTEGER UNIQUE,                    -- id песни ВК, получаемый при загрузке песни
-    status TEXT NOT NULL,                       -- статус обработки песни (будет писаться OK, либо наазвание ошибки)
-    user_id INTEGER NOT NULL,                   -- id пользователя отправившего запрос
-    url TEXT NOT NULL,                          -- url запроса
-    new_title TEXT,                             -- поле для кастомного названия песни
-    new_author TEXT,                            -- поле для кастомного имени автора
-    is_segmented BOOLEAN NOT NULL,              -- флаг сегментирования песни
-    audio_duration INTEGER NOT NULL,            -- длительность песни
-    process_time INTEGER NOT NULL,              -- время обработки запроса
-    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP    -- дата запроса
+CREATE TABLE if not exists users (
+    user_id int PRIMARY KEY,
+    role smallint NOT NULL DEFAULT 0,
+    first_msg_date timestamp(0) NOT NULL DEFAULT current_timestamp,
+    last_msg_date timestamp(0) NOT NULL DEFAULT current_timestamp
 );
 
-CREATE TABLE if not exists users (
-    user_id INTEGER PRIMARY KEY,                    -- id пользователя в вк
-    role INTEGER NOT NULL,                          -- роль пользователя
-    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,   -- дата регистрации пользователя, т.е. отправления первой команды /start
-    last_req_date TIMESTAMP NOT NULL,               -- дата последнего сообщения
-    req_count INTEGER NOT NULL,                     -- общее кол-во запросов (сообщений)
-    is_promoting BOOLEAN NOT NULL                   -- включён ли режим продвижения (название группы в авторстве песни)
+CREATE TABLE if not exists user_settings (
+    user_id int PRIMARY KEY references users(user_id),
+    is_promoting boolean NOT NULL DEFAULT true
+);
+
+
+CREATE TABLE if not exists vk_messages (
+    msg_id int PRIMARY KEY,
+    author_id int NOT NULL references users(user_id),
+    msg_body text NOT NULL,
+    msg_date timestamp(0) NOT NULL DEFAULT current_timestamp
+);
+
+CREATE TABLE if not exists convert_requests (
+    task_id serial PRIMARY KEY,
+    msg_request_id int NOT NULL references vk_messages(msg_id),
+    download_url text NOT NULL,
+    audio_id int references vk_audio(audio_id),
+    status_str text,
+    process_time real(2),
+    request_date timestamp(0) NOT NULL DEFAULT current_timestamp
+);
+
+CREATE TABLE if not exists vk_audio (
+    audio_id int PRIMARY KEY,
+    custom_title text,
+    custom_author text,
+    is_segmented boolean NOT NULL DEFAULT false,
+    audio_duration real(2) NOT NULL
 );
